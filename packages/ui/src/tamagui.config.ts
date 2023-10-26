6fdd9c4db1d149f21a8814c5797a6eae62c89529
michaelessiet <emsaa2002@gmail.com>
2023-10-27 00:19:54 +0100

    added more color themes

--- packages/ui/src/tamagui.config.ts
+++ packages/ui/src/tamagui.config.ts
@@ -1,7 +1,20 @@
 import { tokens } from "@tamagui/themes/v2"
 import { createTamagui, createTokens } from "tamagui"
 import { colors } from "./colors"
-import { dark_brand, light_brand } from "./themes"
+import {
+	dark_brand,
+	light_brand,
+	dark,
+	light,
+	dark_error,
+	dark_neutral,
+	dark_success,
+	dark_warning,
+	light_error,
+	light_neutral,
+	light_success,
+	light_warning,
+} from "./themes"
 
 const myTokens = createTokens({
 	...tokens,
@@ -10,8 +23,18 @@ const myTokens = createTokens({
 
 const appConfig = createTamagui({
 	themes: {
+		dark,
+		light,
 		light_brand,
 		dark_brand,
+		light_error,
+		dark_error,
+		light_success,
+		dark_success,
+		light_neutral,
+		dark_neutral,
+		light_warning,
+		dark_warning
 	},
 	tokens: myTokens,
 })
--- packages/ui/src/themes.ts
+++ packages/ui/src/themes.ts
@@ -4,8 +4,17 @@ import { colors } from "./colors";
 
 // Note: This is a simple implementation of defining themes. If you would like to learn a more concise way, check out https://tamagui.dev/docs/guides/theme-builder 
 
+export const dark: Partial<TamaguiBaseTheme> = {
+	background: colors.black,
+	color: colors.white
+}
+
+export const light: Partial<TamaguiBaseTheme> = {
+	background: colors.black,
+	color: colors.white
+}
+
 export const dark_brand: TamaguiBaseTheme = {
-	...darkColors,
 	background: colors.brand800,
 	backgroundFocus: colors.brand600,
 	backgroundHover: colors.brand700,
@@ -18,14 +27,13 @@ export const dark_brand: TamaguiBaseTheme = {
 	colorFocus: colors.brand300,
 	colorHover: colors.brand400,
 	colorPress: colors.brand300,
-	shadowColor: colors.neutral900,
-	shadowColorFocus: colors.neutral700,
-	shadowColorHover: colors.neutral900,
-	shadowColorPress: colors.neutral700
+	shadowColor: colors.brand900,
+	shadowColorFocus: colors.brand700,
+	shadowColorHover: colors.brand900,
+	shadowColorPress: colors.brand700
 }
 
 export const light_brand: TamaguiBaseTheme = {
-	...lightColors,
 	background: colors.brand100,
 	backgroundFocus: colors.brand300,
 	backgroundHover: colors.brand200,
@@ -38,8 +46,160 @@ export const light_brand: TamaguiBaseTheme = {
 	colorFocus: colors.brand900,
 	colorHover: colors.brand700,
 	colorPress: colors.brand900,
+	shadowColor: colors.brand100,
+	shadowColorFocus: colors.brand300,
+	shadowColorHover: colors.brand100,
+	shadowColorPress: colors.brand300
+}
+
+export const dark_error: TamaguiBaseTheme = {
+	background: colors.error800,
+	backgroundFocus: colors.error600,
+	backgroundHover: colors.error700,
+	backgroundPress: colors.error600,
+	borderColor: colors.error400,
+	borderColorFocus: colors.error500,
+	borderColorHover: colors.error500,
+	borderColorPress: colors.error400,
+	color: colors.error200,
+	colorFocus: colors.error300,
+	colorHover: colors.error400,
+	colorPress: colors.error300,
+	shadowColor: colors.error900,
+	shadowColorFocus: colors.error700,
+	shadowColorHover: colors.error900,
+	shadowColorPress: colors.error700
+}
+
+export const light_error: TamaguiBaseTheme = {
+	background: colors.error100,
+	backgroundFocus: colors.error300,
+	backgroundHover: colors.error200,
+	backgroundPress: colors.error300,
+	borderColor: colors.error400,
+	borderColorFocus: colors.error600,
+	borderColorHover: colors.error600,
+	borderColorPress: colors.error400,
+	color: colors.error400,
+	colorFocus: colors.error900,
+	colorHover: colors.error700,
+	colorPress: colors.error900,
+	shadowColor: colors.error100,
+	shadowColorFocus: colors.error300,
+	shadowColorHover: colors.error100,
+	shadowColorPress: colors.error300
+}
+
+export const dark_success: TamaguiBaseTheme = {
+	background: colors.success800,
+	backgroundFocus: colors.success600,
+	backgroundHover: colors.success700,
+	backgroundPress: colors.success600,
+	borderColor: colors.success400,
+	borderColorFocus: colors.success500,
+	borderColorHover: colors.success500,
+	borderColorPress: colors.success400,
+	color: colors.success200,
+	colorFocus: colors.success300,
+	colorHover: colors.success400,
+	colorPress: colors.success300,
+	shadowColor: colors.success900,
+	shadowColorFocus: colors.success700,
+	shadowColorHover: colors.success900,
+	shadowColorPress: colors.success700
+}
+
+export const light_success: TamaguiBaseTheme = {
+	background: colors.success100,
+	backgroundFocus: colors.success300,
+	backgroundHover: colors.success200,
+	backgroundPress: colors.success300,
+	borderColor: colors.success400,
+	borderColorFocus: colors.success600,
+	borderColorHover: colors.success600,
+	borderColorPress: colors.success400,
+	color: colors.success400,
+	colorFocus: colors.success900,
+	colorHover: colors.success700,
+	colorPress: colors.success900,
+	shadowColor: colors.success100,
+	shadowColorFocus: colors.success300,
+	shadowColorHover: colors.success100,
+	shadowColorPress: colors.success300
+}
+
+export const dark_neutral: TamaguiBaseTheme = {
+	background: colors.neutral800,
+	backgroundFocus: colors.neutral600,
+	backgroundHover: colors.neutral700,
+	backgroundPress: colors.neutral600,
+	borderColor: colors.neutral400,
+	borderColorFocus: colors.neutral500,
+	borderColorHover: colors.neutral500,
+	borderColorPress: colors.neutral400,
+	color: colors.neutral200,
+	colorFocus: colors.neutral300,
+	colorHover: colors.neutral400,
+	colorPress: colors.neutral300,
+	shadowColor: colors.neutral900,
+	shadowColorFocus: colors.neutral700,
+	shadowColorHover: colors.neutral900,
+	shadowColorPress: colors.neutral700
+}
+
+export const light_neutral: TamaguiBaseTheme = {
+	background: colors.neutral100,
+	backgroundFocus: colors.neutral300,
+	backgroundHover: colors.neutral200,
+	backgroundPress: colors.neutral300,
+	borderColor: colors.neutral400,
+	borderColorFocus: colors.neutral600,
+	borderColorHover: colors.neutral600,
+	borderColorPress: colors.neutral400,
+	color: colors.neutral400,
+	colorFocus: colors.neutral900,
+	colorHover: colors.neutral700,
+	colorPress: colors.neutral900,
 	shadowColor: colors.neutral100,
 	shadowColorFocus: colors.neutral300,
 	shadowColorHover: colors.neutral100,
 	shadowColorPress: colors.neutral300
+}
+
+export const dark_warning: TamaguiBaseTheme = {
+	background: colors.warning800,
+	backgroundFocus: colors.warning600,
+	backgroundHover: colors.warning700,
+	backgroundPress: colors.warning600,
+	borderColor: colors.warning400,
+	borderColorFocus: colors.warning500,
+	borderColorHover: colors.warning500,
+	borderColorPress: colors.warning400,
+	color: colors.warning200,
+	colorFocus: colors.warning300,
+	colorHover: colors.warning400,
+	colorPress: colors.warning300,
+	shadowColor: colors.warning900,
+	shadowColorFocus: colors.warning700,
+	shadowColorHover: colors.warning900,
+	shadowColorPress: colors.warning700
+}
+
+export const light_warning: TamaguiBaseTheme = {
+	background: colors.warning100,
+	backgroundFocus: colors.warning300,
+	backgroundHover: colors.warning200,
+	backgroundPress: colors.warning300,
+	borderColor: colors.warning400,
+	borderColorFocus: colors.warning600,
+	borderColorHover: colors.warning600,
+	borderColorPress: colors.warning400,
+	color: colors.warning400,
+	colorFocus: colors.warning900,
+	colorHover: colors.warning700,
+	colorPress: colors.warning900,
+	shadowColor: colors.warning100,
+	shadowColorFocus: colors.warning300,
+	shadowColorHover: colors.warning100,
+	shadowColorPress: colors.warning300
 }
